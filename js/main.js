7a5ea627fa3f7254dafe49c17a5d55c491faf1da
Rohan <rseshadri@team4099.com>
2025-04-28 21:24:26 -0400

    Added functionality for buttons

---
+++ js/main.js
@@ -0,0 +1,20 @@
+console.log('Main JS file connected!');
+function predictNFL() {
+    document.getElementById('prediction-result').innerHTML = "NFL prediction: Coming soon!";
+    console.log("NFL prediction button clicked.");
+}
+
+function predictNBA() {
+    document.getElementById('prediction-result').innerHTML = "NBA prediction: Coming soon!";
+    console.log("NBA prediction button clicked.");
+}
+
+function predictNHL() {
+    document.getElementById('prediction-result').innerHTML = "NHL prediction: Coming soon!";
+    console.log("NHL prediction button clicked.");
+}
+
+function predictMLB() {
+    document.getElementById('prediction-result').innerHTML = "MLB prediction: Coming soon!";
+    console.log("MLB prediction button clicked.");
+}
