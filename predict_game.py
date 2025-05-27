50aac6246e9bc7df94036bf3bddb4e6e89ea84a9
FlockNation <seshrohan@gmail.com>
2025-05-27 16:13:47 -0400

    Update predict_game.py

--- predict_game.py
+++ predict_game.py
@@ -1,12 +1,18 @@
+import joblib
 import numpy as np
-from joblib import load
+import random
 
-def predict_game(team_a_stats, team_b_stats, model_path):
-    model = load(model_path)
-    diff = np.array(team_a_stats) - np.array(team_b_stats)
-    win_prob = model.predict_proba([diff])[0][1]
+def predict_game(team_a_stats, team_b_stats, league):
+    model = joblib.load(f"{league}_model.joblib")
+    scaler = joblib.load(f"{league}_scaler.joblib")
 
-    upset_factor = 0.05
-    adjusted_prob = win_prob * (1 - upset_factor) + (1 - win_prob) * upset_factor
+    features = np.array([team_a_stats['Wins'] - team_b_stats['Wins'],
+                         team_a_stats['Losses'] - team_b_stats['Losses']]).reshape(1, -1)
 
-    return np.random.rand() < adjusted_prob
+    features_scaled = scaler.transform(features)
+    win_prob = model.predict_proba(features_scaled)[0][1]
+
+    upset_chance = 0.1
+    adjusted_prob = win_prob * (1 - upset_chance) + (random.random() * upset_chance)
+
+    return True if adjusted_prob > 0.5 else False
